c4300252775d99c5722b8e2ed49f442b36acdc5f
Rafael Ramirez <randre17@hotmail.com>
2017-01-17 13:07:48 -0500

    Agregar archivos de proyecto.

---
+++ ProyVentas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyVentas.Startup))]
+namespace ProyVentas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
